8893171cd03a8345d67660629488b2f63c26fd78
int-argc <int-argc@users.noreply.github.com>
2016-02-12 15:39:38 +0800

    implemented cache checker and cache content table checker

--- cache_ops.c
+++ cache_ops.c
@@ -6,10 +6,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
+#include <string.h>
 
 #include "global_defs.h"
+#include "cmd_exec.h"
 
 #define DBNAME	"cvfs_db"
+#define CACHELOC "./srcdir"  // use real cache folder
+#define MAX_CACHE_SIZE  3
+
 
 /*
     increments frequency count of a file, this function assumes that
@@ -21,7 +26,7 @@ void incrementFrequency(string filename) {
 	int rc;
     string query = "";
 
-    printf("Opening database %s\n", DBNAME);
+    printf("incrementFrequency(): Opening database %s\n", DBNAME);
 	rc = sqlite3_open(DBNAME, &db);
 	if(rc) {
 		fprintf(stderr, "Can\'t open database %s\n", sqlite3_errmsg(db));
@@ -42,9 +47,95 @@ void incrementFrequency(string filename) {
 
 }
 
+int inCache(string *list, string file) {
+    int i;
+    for(i = 0; i < MAX_CACHE_SIZE; i++) {
+        if (strcmp(list[i], file) == 0) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+static int storeCont(void *cont_p, int argc, char **argv, char **colname) {
+    int i;
+	string comm = "";
+    string *contents = cont_p;   // can result in segmentation fault, do not change
+    // printf("in store cont\n");
+
+	for (i = 0; i < argc; i++) {
+		printf("%s = %s\n", colname[i], argv[i] ? argv[i] : "NULL");
+	}
+
+	if(argc != 3) {
+		fprintf(stderr, "Error in database table: missing columns\n");
+		exit(1);
+	} else {
+        if(inCache(contents, argv[0])) {
+            printf("in cache\n");
+        } else {
+            printf("not in cache\n");
+        }
+	}
+
+	printf("\n");
+	return 0;
+}
+
+void refreshCache() {
+    // get files currently in cache
+    sqlite3 *db;
+    char *errmsg = 0;
+	int rc;
+    string cachefiles = "", comm = "", comm_out = "", file_list = "", query = "";
+    string contents[MAX_CACHE_SIZE];
+
+    sprintf(comm, "ls %s | sed -e ':a;N;$!ba;s/\\n/,/g'", CACHELOC);
+    runCommand(comm, comm_out);
+    // sprintf(file_list, "\"%s", comm_out);
+	// int end = strlen(file_list) - 1;
+	// file_list[end] = '\"';
+    strcpy(file_list, comm_out);
+
+
+    // store list of fils in a struct
+    char *pch = NULL;
+    int i;
+    pch = strtok(file_list, ",");       // replace separator if conflict with file name
+    printf("printing contents:\n");
+    while (pch != NULL) {
+        strcpy(contents[i], pch);
+        printf("%s\n", contents[i]);
+        i++;
+        pch = strtok(NULL, ",");
+    }
+
+    // open database
+	printf("refreshCache(): Opening database %s\n", DBNAME);
+	rc = sqlite3_open(DBNAME, &db);
+	if(rc) {
+		fprintf(stderr, "Can\'t open database %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		exit(1);
+	}
+
+    sprintf(query, "SELECT * FROM CacheContent ORDER BY frequency DESC LIMIT %d;", MAX_CACHE_SIZE);
+
+	rc = sqlite3_exec(db, query, storeCont, contents, &errmsg);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL Error: %s\n", errmsg);
+		sqlite3_free(errmsg);
+	}
+	// close db
+	sqlite3_close(db);
+
+}
+
 // temporary driver
 int main() {
     // test increment: success!
+    refreshCache();
 
 }
 
